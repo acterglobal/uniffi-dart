3afda59f599ebdac06d6570e4b4110f649ccb1a0
Benjamin Kampmann <ben@gnunicorn.org>
2023-06-18 18:53:02 +0200

    add simple arithmetic tests

--- fixtures/arithmetic/src/lib.rs
+++ fixtures/arithmetic/src/lib.rs
@@ -10,4 +10,44 @@ pub fn multiply(left: u32, right: u32) -> u32 {
     left * right
 }
 
+#[uniffi::export]
+pub fn devide(left: u32, right: u32) -> Option<u32> {
+    left.checked_div(right)
+}
+
+#[uniffi::export]
+pub fn add_u8(left: u8, right: u8) -> u8 {
+    left + right
+}
+
+#[uniffi::export]
+pub fn add_u16(left: u16, right: u16) -> u16 {
+    left + right
+}
+
+#[uniffi::export]
+pub fn add_u64(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[uniffi::export]
+pub fn add_i8(left: i8, right: i8) -> i8 {
+    left + right
+}
+
+#[uniffi::export]
+pub fn add_i16(left: i16, right: i16) -> i16 {
+    left + right
+}
+
+#[uniffi::export]
+pub fn add_i32(left: i32, right: i32) -> i32 {
+    left + right
+}
+
+#[uniffi::export]
+pub fn add_i64(left: i64, right: i64) -> i64 {
+    left + right
+}
+
 include!(concat!(env!("OUT_DIR"), "/api.uniffi.rs"));
--- fixtures/arithmetic/test/simple_arithmetic_test.dart
+++ fixtures/arithmetic/test/simple_arithmetic_test.dart
@@ -9,4 +9,26 @@ void main() {
   test('2 * 8 = 16', () {
     expect(api.multiply(2, 8), 16);
   });
+  test('u8', () {
+    expect(api.add_u8(2, 2), 4);
+  });
+  test('u16', () {
+    expect(api.add_u16(2, 2), 4);
+  });
+  test('u64', () {
+    expect(api.add_u64(2, 2), 4);
+  });
+
+  test('i8', () {
+    expect(api.add_i8(2, 2), 4);
+  });
+  test('i16', () {
+    expect(api.add_i16(2, 2), 4);
+  });
+  test('i32', () {
+    expect(api.add_i32(2, 2), 4);
+  });
+  test('i64', () {
+    expect(api.add_i64(2, 2), 4);
+  });
 }
