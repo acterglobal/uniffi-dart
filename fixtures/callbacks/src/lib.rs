77040fbcc0b1689dd40d0e7b8cbeb741d6f6849a
chavic <dev.chavic@gmail.com>
2023-09-25 10:36:24 +0200

    initial callback tests

--- fixtures/callbacks/src/lib.rs
+++ fixtures/callbacks/src/lib.rs
@@ -1,5 +1,134 @@
-use std::sync::Arc;
 use uniffi;
 
-include!(concat!(env!("OUT_DIR"), "/api.uniffi.rs"));
+// A close replication of what's in the original test from the uniffi-rs repo
+#[uniffi::export]
+pub trait ForeignGetters {
+    fn get_bool(&self, v: bool, argument_two: bool) -> Result<bool, SimpleError>;
+    fn get_string(&self, v: String, arg2: bool) -> Result<String, SimpleError>;
+    fn get_option(&self, v: Option<String>, arg2: bool) -> Result<Option<String>, ComplexError>;
+    fn get_list(&self, v: Vec<i32>, arg2: bool) -> Result<Vec<i32>, SimpleError>;
+    fn get_nothing(&self, v: String) -> Result<(), SimpleError>;
+}
 
+// Dart can throw any non-null object as an exception
+#[derive(Debug, thiserror::Error, uniffi::Error)]
+pub enum SimpleError {
+    #[error("BadArgument")]
+    BadArgument,
+    #[error("InternalTelephoneError")]
+    UnexpectedError,
+}
+
+
+#[derive(Debug, thiserror::Error, uniffi::Error)]
+pub enum ComplexError {
+    #[error("ReallyBadArgument")]
+    ReallyBadArgument { code: i32 },
+    #[error("InternalTelephoneError")]
+    UnexpectedErrorWithReason { reason: String },
+}
+
+impl From<uniffi::UnexpectedUniFFICallbackError> for SimpleError {
+    fn from(e: uniffi::UnexpectedUniFFICallbackError) -> SimpleError {
+        SimpleError::UnexpectedError
+    }
+}
+
+
+impl From<uniffi::UnexpectedUniFFICallbackError> for ComplexError {
+    fn from(e: uniffi::UnexpectedUniFFICallbackError) -> ComplexError {
+        ComplexError::UnexpectedErrorWithReason { reason: e.reason }
+    }
+}
+
+
+#[derive(Debug, Clone, uniffi::Object)]
+pub struct RustGetters;
+
+// TODO: solve the type error when using the export proc_macro
+//#[uniffi::export]
+impl RustGetters {
+   #[uniffi::constructor]
+    pub fn new() -> Self {
+        RustGetters
+    }
+    pub fn get_bool(
+        &self,
+        callback: Box<dyn ForeignGetters>,
+        v: bool,
+        argument_two: bool,
+    ) -> Result<bool, SimpleError> {
+        callback.get_bool(v, argument_two)
+    }
+    pub fn get_string(
+        &self,
+        callback: Box<dyn ForeignGetters>,
+        v: String,
+        arg2: bool,
+    ) -> Result<String, SimpleError> {
+        callback.get_string(v, arg2)
+    }
+    pub fn get_option(
+        &self,
+        callback: Box<dyn ForeignGetters>,
+        v: Option<String>,
+        arg2: bool,
+    ) -> Result<Option<String>, ComplexError> {
+        callback.get_option(v, arg2)
+    }
+   pub fn get_list(
+        &self,
+        callback: Box<dyn ForeignGetters>,
+        v: Vec<i32>,
+        arg2: bool,
+    ) -> Result<Vec<i32>, SimpleError> {
+        callback.get_list(v, arg2)
+    }
+
+   pub fn get_string_optional_callback(
+        &self,
+        callback: Option<Box<dyn ForeignGetters>>,
+        v: String,
+        arg2: bool,
+    ) -> Result<Option<String>, SimpleError> {
+        callback.map(|c| c.get_string(v, arg2)).transpose()
+    }
+
+   pub fn get_nothing(&self, callback: Box<dyn ForeignGetters>, v: String) -> Result<(), SimpleError> {
+        callback.get_nothing(v)
+    }
+}
+
+#[uniffi::export]
+impl Default for RustGetters {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[allow(clippy::wrong_self_convention)]
+#[uniffi::export]
+trait StoredForeignStringifier: Send + Sync + std::fmt::Debug {
+    fn from_simple_type(&self, value: i32) -> String;
+    fn from_complex_type(&self, values: Option<Vec<Option<f64>>>) -> String;
+}
+
+#[derive(Debug, uniffi::Object)]
+pub struct RustStringifier {
+    callback: Box<dyn StoredForeignStringifier>,
+}
+
+#[uniffi::export]
+impl RustStringifier {
+    fn new(callback: Box<dyn StoredForeignStringifier>) -> Self {
+        RustStringifier { callback }
+    }
+
+    #[allow(clippy::wrong_self_convention)]
+    fn from_simple_type(&self, value: i32) -> String {
+        self.callback.from_simple_type(value)
+    }
+}
+
+
+include!(concat!(env!("OUT_DIR"), "/api.uniffi.rs"));
--- fixtures/callbacks/test/callbacks_test.dart
+++ fixtures/callbacks/test/callbacks_test.dart
@@ -1,7 +1,164 @@
 import 'package:test/test.dart';
+import 'dart:ffi';
 import '../callbacks.dart';
+
+class DartGetters extends ForeignGetters {
+  @override
+  bool getBool(bool v, bool argumentTwo) => v ^ argumentTwo;
+
+  @override
+  String getString(String v, bool arg2) {
+    if (v == 'bad-argument') {
+      throw Exception('bad argument');
+    }
+    if (v == 'unexpected-error') {
+      throw Exception('something failed');
+    }
+    return arg2 ? '1234567890123' : v;
+  }
+
+  @override
+  String? getOption(String? v, bool arg2) {
+    if (v == 'bad-argument') {
+      throw Exception('bad argument');
+    }
+    if (v == 'unexpected-error') {
+      throw Exception('something failed');
+    }
+    return arg2 ? v?.toUpperCase() : v;
+  }
+
+  @override
+  List<int> getList(Pointer<Int32> v, bool arg2) =>
+      arg2 ? List<int>.from(v.asTypedList(0)) : [];
+
+  @override
+  void getNothing(String v) {
+    if (v == 'bad-argument') {
+      throw Exception('bad argument');
+    }
+    if (v == 'unexpected-error') {
+      throw Exception('something failed');
+    }
+  }
+}
+
+class StoredDartStringifier extends StoredForeignStringifier {
+  @override
+  String fromSimpleType(int value) => 'kotlin: $value';
+
+  // We don't test this, but we're checking that the arg type is included in the minimal list of types used
+  // in the UDL.
+  // If this doesn't compile, then look at TypeResolver.
+  String fromComplexType(List<double?>? values) => 'kotlin: $values';
+}
+
 
 void main() {
   final api = Api.load();
-  
+  final callback = DartGetters();
+  final rustGetters = RustGetters();
+
+  // 1. Testing callback methods
+  final bool flag = true;
+
+  for (final v in [true, false]) {
+    final expected = callback.getBool(v, flag);
+    final observed = rustGetters.getBool(callback, v, flag);
+    assert(expected == observed,
+        "roundtripping through callback: $expected != $observed");
+  }
+
+  for (final v in [
+    [1, 2],
+    [0, 1]
+  ]) {
+    final expected = callback.getList(v, flag);
+    final observed = rustGetters.getList(callback, v, flag);
+    assert(expected == observed,
+        "roundtripping through callback: $expected != $observed");
+  }
+
+  for (final v in ["Hello", "world"]) {
+    final expected = callback.getString(v, flag);
+    final observed = rustGetters.getString(callback, v, flag);
+    assert(expected == observed,
+        "roundtripping through callback: $expected != $observed");
+  }
+
+  for (final v in ["Some", null]) {
+    final expected = callback.getOption(v, !flag);
+    final observed = rustGetters.getOption(callback, v, !flag);
+    assert(expected == observed,
+        "roundtripping through callback: $expected != $observed");
+  }
+
+  // Additional tests
+  assert(rustGetters.getStringOptionalCallback(callback, "TestString", false) ==
+      "TestString");
+  assert(rustGetters.getStringOptionalCallback(nullptr, "TestString", false) ==
+      nullptr);
+
+  // Should not throw
+  rustGetters.getNothing(callback, "TestString");
+
+  // Exception handling
+  try {
+    rustGetters.getString(callback, "bad-argument", true);
+    throw Exception("Expected SimpleException.BadArgument");
+  } on SimpleException.BadArgument {
+    // Expected error
+  }
+
+  try {
+    rustGetters.getString(callback, "unexpected-error", true);
+    throw Exception("Expected SimpleException.UnexpectedException");
+  } on SimpleException.UnexpectedException {
+    // Expected error
+  }
+
+  try {
+    rustGetters.getOption(callback, "bad-argument", true);
+    throw Exception("Expected ComplexException.ReallyBadArgument");
+  } on ComplexException.ReallyBadArgument catch (e) {
+    // Expected error
+    assert(e.code == 20);
+  }
+
+  try {
+    rustGetters.getOption(callback, "unexpected-error", true);
+    throw Exception("Expected ComplexException.UnexpectedErrorWithReason");
+  } on ComplexException.UnexpectedErrorWithReason catch (e) {
+    // Expected error
+    assert(e.reason == Exception("something failed").toString());
+  }
+
+  try {
+    rustGetters.getNothing(callback, "bad-argument");
+    throw Exception("Expected SimpleException.BadArgument");
+  } on SimpleException.BadArgument {
+    // Expected error
+  }
+
+  try {
+    rustGetters.getNothing(callback, "unexpected-error");
+    throw Exception("Expected SimpleException.UnexpectedException");
+  } on SimpleException.UnexpectedException {
+    // Expected error
+  }
+
+  rustGetters.destroy();
+
+  // 2. Pass the callback in as a constructor argument
+  final DartStringifier = StoredDartStringifier();
+  final rustStringifier = RustStringifier(DartStringifier);
+
+  for (final v in [1, 2]) {
+    final expected = DartStringifier.fromSimpleType(v);
+    final observed = rustStringifier.fromSimpleType(v);
+    assert(expected == observed,
+        "callback is sent on construction: $expected != $observed");
+  }
+
+  rustStringifier.destroy();
 }
