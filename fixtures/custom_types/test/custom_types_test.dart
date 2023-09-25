c792db4fb4c332f8c34558ae9ed4a8facf101a1c
chavic <dev.chavic@gmail.com>
2023-09-25 11:05:23 +0200

    initial custom types

--- fixtures/custom_types/test/custom_types_test.dart
+++ fixtures/custom_types/test/custom_types_test.dart
@@ -4,4 +4,14 @@ import '../custom_types.dart';
 void main() {
   final api = Api.load();
   
+  final demo = api.getCustomTypesDemo(null);
+
+  assert(demo.url.toString() == 'http://example.com/');
+  
+  assert(demo.handle == 123);
+
+  demo.url = Uri.parse('http://new.example.com/');
+  demo.handle = 456;
+  
+  assert(demo == api.getCustomTypesDemo(demo));
 }
