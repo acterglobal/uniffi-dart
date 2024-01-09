b354228062b24ae92d320bff562e143da668c00d
chavic <dev.chavic@gmail.com>
2024-01-09 10:28:27 +0200

    added docs

--- src/gen/render/mod.rs
+++ src/gen/render/mod.rs
@@ -1,14 +1,21 @@
 use genco::lang::dart;
 use uniffi_bindgen::interface::Type;
 
-mod primitives;
-mod enums;
+pub(crate) mod primitives;
+pub(crate) mod enums;
 
+/// This trait will be used by any type that generates dart code according to some logic, 
+/// i.e `TypeRender` handles internal and external imports and type helpers
 pub trait Renderer {
      fn render(&self, r: &impl Renderable) -> dart::Tokens;
 }
 
+/// This trait is used by types that should be generated. The idea is to pass any struct that implements 
+/// this type to another struct that generates much larger portions of according to some internal logic code 
+/// and implements `Renderer`.
 pub trait Renderable {
+     /// Renders the code that defines a type
      fn render_type(&self, ty: &Type) -> dart::Tokens;
+     /// Renders code for type helpers for lifting, lowering, buffer conversion, etc...
      fn render_type_helpers(&self, ty: &Type) -> dart::Tokens;
 }
