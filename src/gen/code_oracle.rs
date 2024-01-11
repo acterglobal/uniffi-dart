5375ea9b5816eea655a44d720c5daa95e8315b9d
chavic <dev.chavic@gmail.com>
2024-01-11 16:18:23 +0200

    Add uniffi_bindgen dependencies and import statements.

--- src/gen/code_oracle.rs
+++ src/gen/code_oracle.rs
@@ -1,5 +1,8 @@
 use heck::{ToLowerCamelCase, ToUpperCamelCase};
 
+use uniffi_bindgen::backend::{CodeType, FfiType};
+use uniffi_bindgen::interface::{Type, Literal};
+
 pub struct DartCodeOracle;
 
 impl DartCodeOracle {
