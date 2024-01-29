9c060ee91e329d4ceff07b249b9ec4902b0eb5cf
chavic <dev.chavic@gmail.com>
2024-01-29 11:55:45 +0200

    implemented render for primitives

--- src/gen/primitives.rs
+++ src/gen/primitives.rs
@@ -85,22 +85,200 @@ macro_rules! impl_code_type_for_primitive {
                     format!("{}.read", self.ffi_converter_name())
                 }
             }
+        }
+    };
+}
 
-            impl Renderable for $T {
-                fn render_type(&self, _ty: &Type) -> dart::Tokens {
-                    quote!($class_name)
-                }
+macro_rules! impl_renderable_for_primitive {
+    ($T:ty, $class_name:literal, $canonical_name:literal, $allocation_size:literal) => {
+        impl Renderable for $T {
+            fn render_type(&self, _ty: &Type) -> dart::Tokens {
+                quote!($class_name)
+            }
 
-                fn render_type_helpers(&self, _ty: &Type) -> dart::Tokens {
-                    // This method can be expanded to generate type helper methods if needed.
-                    quote! {
-                        //TODO: Make each render_type impliment `FfiConverter`
-                    }
+            fn render_type_helpers(&self, _ty: &Type) -> dart::Tokens {
+                // This method can be expanded to generate type helper methods if needed.
+                let endian = (if $canonical_name.contains("Float") { "Endian.little" } else { "Endian.big" });
+                let final_uintlist = (if $canonical_name.contains("Float") { 
+                    String::from($canonical_name) + "List.fromList(buf.reversed.toList())" 
+                } else {
+                    String::from($canonical_name) + "List.fromList(buf.toList())" 
+                });
+
+                quote! {
+                    class IntFfiConverter extends FfiConverter<int, RustBuffer> {
+                        @override
+                        int lift(Api api, RustBuffer buf) {
+                            final uint_list = buf.toIntList();
+                            return uint_list.buffer.asByteData().get$canonical_name(1);
+                        }
+                      
+                        @override
+                        RustBuffer lower(Api api, int value) {
+                            final buf = Uint8List(this.allocationSize());
+                            final byteData = ByteData.sublistView(buf);
+                            byteData.set$canonical_name(0, value, $endian);
+                            return toRustBuffer($final_uintlist);
+                        }
+                      
+                        @override
+                        int read(ByteBuffer buf) {
+                            // So here's the deal, we have two choices, could use Uint8List or ByteBuffer, leaving this for later
+                            // considerations, after research on performance implications
+                          throw UnimplementedError("Should probably impliment read now");
+                        }
+                      
+                        @override
+                        int allocationSize(int value) {
+                          return $allocation_size; 
+                        }
+                      
+                        @override
+                        void write(int value, ByteBuffer buf) {
+                            throw UnimplementedError("Should probably impliment writes now");
+                        }
+                    }                      
                 }
             }
         }
     };
+
+    (BooleanCodeType, $class_name:literal, $canonical_name:literal, $allocation_size:literal) => {
+        impl Renderable for $T {
+            fn render_type(&self, _ty: &Type) -> dart::Tokens {
+                quote!($class_name)
+            }
+
+            fn render_type_helpers(&self, _ty: &Type) -> dart::Tokens {
+                // This method can be expanded to generate type helper methods if needed.
+                quote! {
+                    class IntFfiConverter extends FfiConverter<int, RustBuffer> {
+                        @override
+                        bool lift(Api api, RustBuffer buf) {
+                            final uint_list = buf.toIntList();
+                            return uint_list.buffer.sublist(offset)[0] == 1;
+                        }
+                      
+                        @override
+                        RustBuffer lower(Api api, bool value) {
+                            final uint_list = Uint8List.fromList([value ? 1 : 0]);
+                            return toRustBuffer(uint_list);
+                        }
+                      
+                        @override
+                        int read(ByteBuffer buf) {
+                            // So here's the deal, we have two choices, could use Uint8List or ByteBuffer, leaving this for later
+                            // performance reasons
+                          throw UnimplementedError("Should probably impliment read now");
+                        }
+                      
+                        @override
+                        int allocationSize(int value) {
+                          return $allocation_size; // 1 = 8bits
+                        }
+                      
+                        @override
+                        void write(int value, ByteBuffer buf) {
+                            throw UnimplementedError("Should probably impliment read now");
+                        }
+                    }                 
+                }
+            }
+        }
+    };
+    
+    (StringCodeType, $class_name:literal, $canonical_name:literal, $allocation_size:literal) => {
+        impl Renderable for $T {
+            fn render_type(&self, _ty: &Type) -> dart::Tokens {
+                quote!($class_name)
+            }
+
+            fn render_type_helpers(&self, _ty: &Type) -> dart::Tokens {
+                // This method can be expanded to generate type helper methods if needed.
+                quote! {
+                    class IntFfiConverter extends FfiConverter<int, RustBuffer> {
+                        @override
+                        int lift(Api api, RustBuffer buf) {
+                            final uint_list = buf.toIntList();
+                            return utf8.decoder.convert(uint_list);
+                        }
+                      
+                        @override
+                        RustBuffer lower(Api api, String value) {
+                            // FIXME: this is too many memcopies!
+                            return Utf8Encoder().convert(input);
+                        }
+                      
+                        @override
+                        int read(ByteBuffer buf) {
+                            // So here's the deal, we have two choices, could use Uint8List or ByteBuffer, leaving this for later
+                            // performance reasons
+                          throw UnimplementedError("Should probably impliment read now");
+                        }
+                      
+                        @override
+                        int allocationSize(int value) {
+                          return $allocation_size; // 1 = 8bits
+                        }
+                      
+                        @override
+                        void write(int value, ByteBuffer buf) {
+                            throw UnimplementedError("Should probably impliment read now");
+                        }
+                    }                 
+                }
+            }
+        }
+    };
+
+    (BytesCodeType, $class_name:literal, $canonical_name:literal, $allocation_size:literal) => {
+        impl Renderable for $T {
+            fn render_type(&self, _ty: &Type) -> dart::Tokens {
+                quote!($class_name)
+            }
+
+            fn render_type_helpers(&self, _ty: &Type) -> dart::Tokens {
+                // This method can be expanded to generate type helper methods if needed.
+                quote! {
+                    class IntFfiConverter extends FfiConverter<int, RustBuffer> {
+                        @override
+                        int lift(Api api, RustBuffer buf) {
+                            final uint_list = buf.toIntList();
+                            return uint_list.buffer.asByteData().get$canonical_name(1);
+                        }
+                      
+                        @override
+                        RustBuffer lower(Api api, int value) {
+                            final uint_list = Uint8List.fromList([value ? 1 : 0]);
+                            final byteData = ByteData.sublistView(buf);
+                            byteData.setInt16(0, value, Endian.little);
+                            return buf;
+                        }
+                      
+                        @override
+                        int read(ByteBuffer buf) {
+                            // So here's the deal, we have two choices, could use Uint8List or ByteBuffer, leaving this for later
+                            // performance reasons
+                          throw UnimplementedError("Should probably impliment read now");
+                        }
+                      
+                        @override
+                        int allocationSize(int value) {
+                          return $allocation_size; // 1 = 8bits
+                        }
+                      
+                        @override
+                        void write(int value, ByteBuffer buf) {
+                            throw UnimplementedError("Should probably impliment read now");
+                        }
+                    }                 
+                }
+            }
+        }
+    }
 }
+
+// TODO: Impliment separate for string
 
 impl_code_type_for_primitive!(BooleanCodeType, "bool", "Bool");
 impl_code_type_for_primitive!(StringCodeType, "String", "String");
@@ -115,3 +293,18 @@ impl_code_type_for_primitive!(UInt32CodeType, "int", "UInt32");
 impl_code_type_for_primitive!(UInt64CodeType, "int", "UInt64");
 impl_code_type_for_primitive!(Float32CodeType, "double", "Double32");
 impl_code_type_for_primitive!(Float64CodeType, "double", "Double64");
+
+impl_renderable_for_primitive!(BooleanCodeType, "bool", "Bool", 1);
+impl_renderable_for_primitive!(StringCodeType, "String", "String", 1);
+// TODO: Impliment BytesCodeType
+// impl_renderable_for_primitive!(BytesCodeType, "Uint8List", "Uint8List", 1);
+impl_renderable_for_primitive!(Int8CodeType, "int", "Int8", 1);
+impl_renderable_for_primitive!(Int16CodeType, "int", "Int16", 2);
+impl_renderable_for_primitive!(Int32CodeType, "int", "Int32", 4);
+impl_renderable_for_primitive!(Int64CodeType, "int", "Int64", 8);
+impl_renderable_for_primitive!(UInt8CodeType, "int", "UInt8", 1);
+impl_renderable_for_primitive!(UInt16CodeType, "int", "UInt16", 2);
+impl_renderable_for_primitive!(UInt32CodeType, "int", "UInt32", 4);
+impl_renderable_for_primitive!(UInt64CodeType, "int", "UInt64", 8);
+impl_renderable_for_primitive!(Float32CodeType, "double", "Double32", 4);
+impl_renderable_for_primitive!(Float64CodeType, "double", "Double64", 8);
