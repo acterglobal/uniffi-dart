10ce63c470b914c3448e906d6b337b5ec719c085
chavic <dev.chavic@gmail.com>
2024-02-12 12:55:59 +0200

    added helper for adding imports

--- src/gen/render/mod.rs
+++ src/gen/render/mod.rs
@@ -9,8 +9,13 @@ pub trait Renderer {
      fn render(&self) -> dart::Tokens;
 }
 
+// This trait contains helpful methods for rendering type helpers
 pub trait TypeHelperRenderer {
+     // Gives context about weather a type's helper code has already been included
      fn include_once_check(&self, name: &str) -> bool;
+     // Helps type helper functions specify a required imports should be added
+     fn add_import(&self, name: &str) -> bool;
+     fn add_import_as(&self, name: &str, as_name: &str) -> bool;
 }
 /// This trait is used by types that should be generated. The idea is to pass any struct that implements 
 /// this type to another struct that generates much larger portions of according to some internal logic code 
