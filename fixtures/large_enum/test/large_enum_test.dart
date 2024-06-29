eeb219338ba6ed6e327b6dccea81e49bb1919f69
chavic <dev.chavic@gmail.com>
2024-06-29 12:21:13 +0200

    removed api

--- fixtures/large_enum/test/large_enum_test.dart
+++ fixtures/large_enum/test/large_enum_test.dart
@@ -2,23 +2,21 @@ import 'package:test/test.dart';
 import '../large_enum.dart';
 
 void main() {
-  final api = Api.load();
-
   // Testing flat enums
   test('Creating/Lifting Flat Enums', () {
     // Do we get the expected enum
-    expect(FlatEnum.one, api.newFlatOne());
-    expect(FlatEnum.two, api.newFlatTwo());
-    expect(FlatEnum.three, api.newFlatThree());
-    expect(FlatEnum.four, api.newFlatFour());
+    expect(FlatEnum.one, newFlatOne());
+    expect(FlatEnum.two, newFlatTwo());
+    expect(FlatEnum.three, newFlatThree());
+    expect(FlatEnum.four, newFlatFour());
   });
 
   test('Passing Down/Lowering Flat Enums', () {
     // Can we pass the value down to rust correctly?
-    expect(api.takeFlatEnum(FlatEnum.one), "One");
-    expect(api.takeFlatEnum(FlatEnum.two), "Two");
-    expect(api.takeFlatEnum(FlatEnum.three), "Three");
-    expect(api.takeFlatEnum(FlatEnum.four), "Four");
+    expect(takeFlatEnum(FlatEnum.one), "One");
+    expect(takeFlatEnum(FlatEnum.two), "Two");
+    expect(takeFlatEnum(FlatEnum.three), "Three");
+    expect(takeFlatEnum(FlatEnum.four), "Four");
   });
 
   // Testing the complex associative types...
@@ -33,27 +31,34 @@ void main() {
   final inner_list = [3, 4, 4, 5, 4, 24434398, 4];
 
   // TODO: Collections (Maps, Vector, ...)
-  U8Value u8Value = (api.newU8Value(inner_value_small) as U8Value);
-  U16Value u16Value = (api.newU16Value(inner_value2) as U16Value);
-  I8Value i8Value = (api.newI8Value(inner_value_small) as I8Value);
-  I16Value i16Value = (api.newI16Value(inner_value2) as I16Value);
+  U8Value u8Value = (newU8Value(inner_value_small) as U8Value);
+  U16Value u16Value = (newU16Value(inner_value2) as U16Value);
+  I8Value i8Value = (newI8Value(inner_value_small) as I8Value);
+  I16Value i16Value = (newI16Value(inner_value2) as I16Value);
 
-  U32Value u32Value = (api.newU32Value(inner_value2) as U32Value);
-  U64Value u64Value = (api.newU64Value(inner_value) as U64Value);
-  I64Value i64Value = (api.newI64Value(inner_value) as I64Value);
-  I32Value i32Value = (api.newI32Value(inner_value2) as I32Value);
-  F32Value f32Value = (api.newF32Value(inner_value_float) as F32Value);
-  F64Value f64Value = (api.newF64Value(inner_value_double) as F64Value);
+  // final mapEntry = {
+  //   'u8Value': u8Value,
+  //   'u16Value': u16Value,
+  //   'i8Value': i8Value,
+  // };
+
+  U32Value u32Value = (newU32Value(inner_value2) as U32Value);
+  U64Value u64Value = (newU64Value(inner_value) as U64Value);
+  I64Value i64Value = (newI64Value(inner_value) as I64Value);
+  I32Value i32Value = (newI32Value(inner_value2) as I32Value);
+  F32Value f32Value = (newF32Value(inner_value_float) as F32Value);
+  F64Value f64Value = (newF64Value(inner_value_double) as F64Value);
 
   StringValue stringValue =
-      (api.newStringValue(inner_value.toString()) as StringValue);
-  BoolValue boolValue = (api.newBoolValue(inner_bool) as BoolValue);
+      (newStringValue(inner_value.toString()) as StringValue);
+  BoolValue boolValue = (newBoolValue(inner_bool) as BoolValue);
 
+  // MapEntry newMapEntry = (newMap(mapEntry) as MapEntry);
   // PublicKeyValue publicKeyValue =
-  //     (api.newPublicKeyValue(inner_list) as PublicKeyValue);
+  //     (newPublicKeyValue(inner_list) as PublicKeyValue);
 
   PublicKeyValue publicKeyValue =
-      (api.newPublicKeyValueWithoutArgument() as PublicKeyValue);
+      (newPublicKeyValueWithoutArgument() as PublicKeyValue);
 
   test('Creating/Lifting Complex Enums', () {
     // Do we get the expected inner value? Correctly.
@@ -74,24 +79,25 @@ void main() {
     expect(boolValue.value, inner_bool);
     // Collections
     expect(publicKeyValue.value, inner_list);
+    // expect(newMapEntry, mapEntry);
   });
 
   test('Passing Down/Lowering Complex Enums', () {
     // Can we pass the value down to rust correctly?
-    expect(api.takeValue(u8Value), inner_value_small.toString());
-    expect(api.takeValue(u16Value), inner_value2.toString());
-    expect(api.takeValue(i8Value), inner_value_small.toString());
-    expect(api.takeValue(i16Value), inner_value2.toString());
-    expect(api.takeValue(u32Value), inner_value2.toString());
-    expect(api.takeValue(i64Value), inner_value.toString());
-    expect(api.takeValue(u64Value), inner_value.toString());
-    expect(api.takeValue(i32Value), inner_value2.toString());
-    expect(api.takeValue(f32Value), inner_value_float.toString());
-    expect(api.takeValue(f64Value), inner_value_double.toString());
+    expect(takeValue(u8Value), inner_value_small.toString());
+    expect(takeValue(u16Value), inner_value2.toString());
+    expect(takeValue(i8Value), inner_value_small.toString());
+    expect(takeValue(i16Value), inner_value2.toString());
+    expect(takeValue(u32Value), inner_value2.toString());
+    expect(takeValue(i64Value), inner_value.toString());
+    expect(takeValue(u64Value), inner_value.toString());
+    expect(takeValue(i32Value), inner_value2.toString());
+    expect(takeValue(f32Value), inner_value_float.toString());
+    expect(takeValue(f64Value), inner_value_double.toString());
 
-    expect(api.takeValue(stringValue), inner_value.toString());
-    expect(api.takeValue(boolValue), inner_bool.toString());
+    expect(takeValue(stringValue), inner_value.toString());
+    expect(takeValue(boolValue), inner_bool.toString());
 
-    expect(api.takeValue(publicKeyValue), inner_list.toString());
+    expect(takeValue(publicKeyValue), inner_list.toString());
   });
 }
