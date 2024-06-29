c1d38f269fbda69351772566f8ab93332e234c90
chavic <dev.chavic@gmail.com>
2024-06-29 10:21:24 +0200

    moved code to mod

--- src/gen/mod.rs
+++ src/gen/mod.rs
@@ -9,6 +9,8 @@ use serde::{Deserialize, Serialize};
 // use uniffi_bindgen::MergeWith;
 use uniffi_bindgen::{BindingGenerator, BindingsConfig, ComponentInterface};
 
+use crate::gen::oracle::DartCodeOracle;
+
 use self::render::Renderer;
 use self::types::TypeHelpersRenderer;
 
@@ -99,12 +101,19 @@ impl BindingsConfig for Config {
 
 pub struct DartWrapper<'a> {
     config: &'a Config,
-    // ci: &'a ComponentInterface,
+    ci: &'a ComponentInterface,
     type_renderer: TypeHelpersRenderer<'a>,
 }
 
 impl<'a> DartWrapper<'a> {
-    // ... (previous methods remain the same)
+    pub fn new(ci: &'a ComponentInterface, config: &'a Config) -> Self {
+        let type_renderer = TypeHelpersRenderer::new(config, ci);
+        DartWrapper {
+            ci,
+            config,
+            type_renderer,
+        }
+    }
 
     fn generate(&self) -> dart::Tokens {
         let package_name = self.config.package_name();
@@ -112,15 +121,61 @@ impl<'a> DartWrapper<'a> {
 
         let (type_helper_code, functions_definitions) = &self.type_renderer.render();
 
+        fn uniffi_function_definitions(ci: &ComponentInterface) -> dart::Tokens {
+            let mut definitions = quote!();
+
+            for fun in ci.iter_ffi_function_definitions() {
+                let fun_name = fun.name();
+                let (native_return_type, dart_return_type) = match fun.return_type() {
+                    Some(return_type) => (
+                        quote! { $(DartCodeOracle::ffi_native_type_label(Some(&return_type))) },
+                        quote! { $(DartCodeOracle::ffi_dart_type_label(Some(&return_type))) },
+                    ),
+                    None => (quote! { Void }, quote! { void }),
+                };
+
+                let (native_args, dart_args) = {
+                    let mut native_args = quote!();
+                    let mut dart_args = quote!();
+
+                    for arg in fun.arguments() {
+                        native_args.append(
+                            quote!($(DartCodeOracle::ffi_native_type_label(Some(&arg.type_()))),),
+                        );
+                        dart_args.append(
+                            quote!($(DartCodeOracle::ffi_dart_type_label(Some(&arg.type_()))),),
+                        );
+                    }
+
+                    if fun.has_rust_call_status_arg() {
+                        native_args.append(quote!(Pointer<RustCallStatus>));
+                        dart_args.append(quote!(Pointer<RustCallStatus>));
+                    }
+
+                    (native_args, dart_args)
+                };
+
+                let lookup_fn = quote! {
+                    _dylib.lookupFunction<
+                        $native_return_type Function($(&native_args)),
+                        $(&dart_return_type) Function($(&dart_args))
+                    >($(format!("\"{fun_name}\"")))
+                };
+
+                definitions.append(quote! {
+                    late final $dart_return_type Function($dart_args) $fun_name = $lookup_fn;
+                });
+            }
+
+            definitions
+        }
+
         quote! {
             library $package_name;
 
-            import 'dart:async';
-            import 'dart:ffi';
-            import 'dart:typed_data';
-            import 'package:ffi/ffi.dart';
-
             $(type_helper_code) // Imports, Types and Type Helper
+
+            $(functions_definitions)
 
             class _UniffiLib {
                 _UniffiLib._();
@@ -128,102 +183,94 @@ impl<'a> DartWrapper<'a> {
                 static final DynamicLibrary _dylib = _open();
 
                 static DynamicLibrary _open() {
-                    if (Platform.isAndroid) return DynamicLibrary.open($(format!("\"lib{libname}.so\"")));
-                    if (Platform.isIOS) return DynamicLibrary.executable();
-                    if (Platform.isLinux) return DynamicLibrary.open($(format!("\"lib{libname}.so\"")));
-                    if (Platform.isMacOS) return DynamicLibrary.open($(format!("\"lib{libname}.dylib\"")));
-                    if (Platform.isWindows) return DynamicLibrary.open($(format!("\"{libname}.dll\"")));
-                    throw UnsupportedError("Unsupported platform: ${Platform.operatingSystem}");
+                  if (Platform.isAndroid) return DynamicLibrary.open($(format!("\"lib{libname}.so\"")));
+                  if (Platform.isIOS) return DynamicLibrary.executable();
+                  if (Platform.isLinux) return DynamicLibrary.open($(format!("\"lib{libname}.so\"")));
+                  if (Platform.isMacOS) return DynamicLibrary.open($(format!("\"lib{libname}.dylib\"")));
+                  if (Platform.isWindows) return DynamicLibrary.open($(format!("\"{libname}.dll\"")));
+                  throw UnsupportedError("Unsupported platform: ${Platform.operatingSystem}");
                 }
 
                 static final _UniffiLib instance = _UniffiLib._();
 
-                $(functions_definitions)
+                $(uniffi_function_definitions(self.ci))
 
-                // ... (FFI function definitions)
+                static void _checkApiVersion() {
+                    final bindingsVersion = $(self.ci.uniffi_contract_version());
+                    final scaffoldingVersion = _UniffiLib.instance.$(self.ci.ffi_uniffi_contract_version().name())();
+                    if (bindingsVersion != scaffoldingVersion) {
+                      throw UniffiInternalError.panicked("UniFFI contract version mismatch: bindings version $bindingsVersion, scaffolding version $scaffoldingVersion");
+                    }
+                }
+
+                static void _checkApiChecksums() {
+                    $(for (name, expected_checksum) in self.ci.iter_checksums() =>
+                        if (_UniffiLib.instance.$(name)() != $expected_checksum) {
+                          throw UniffiInternalError.panicked("UniFFI API checksum mismatch");
+                        }
+                    )
+                }
             }
 
             void initialize() {
                 _UniffiLib._open();
             }
 
-            $(self.generate_helper_functions())
-        }
-    }
-
-    fn generate_helper_functions(&self) -> dart::Tokens {
-        quote! {
-            T rustCall<T>(T Function(Pointer<RustCallStatus>) callback) {
-                final status = calloc<RustCallStatus>();
-                try {
-                    final result = callback(status);
-                    final code = status.ref.code;
-                    switch (code) {
-                        case 0:  // UNIFFI_SUCCESS
-                            return result;
-                        case 1:  // UNIFFI_ERROR
-                            throw status.ref.errorBuf.consumeIntoString();
-                        case 2:  // UNIFFI_PANIC
-                            final message = status.ref.errorBuf.consumeIntoString();
-                            throw UniffiInternalError.panicked(message);
-                        default:
-                            throw UniffiInternalError.unknownCodec(code);
-                    }
-                } finally {
-                    calloc.free(status);
-                }
-            }
-
-            Future<T> uniffiRustCallAsync<T, F>(
-                int Function() rustFutureFunc,
-                void Function(int, Pointer<NativeFunction<UniffiRustFutureContinuationCallback>>, int) pollFunc,
-                F Function(int, Pointer<RustCallStatus>) completeFunc,
-                void Function(int) freeFunc,
-                T Function(F) liftFunc, [
-                UniffiRustCallStatusErrorHandler? errorHandler,
-            ]) async {
-                final rustFuture = rustFutureFunc();
-                final completer = Completer<int>();
-
-                late final NativeCallable<UniffiRustFutureContinuationCallback> callback;
-
-                void poll() {
-                    pollFunc(
-                        rustFuture,
-                        callback.nativeFunction,
-                        0,
-                    );
-                }
-                void onResponse(int _idx, int pollResult) {
-                    if (pollResult == 0) {  // UNIFFI_POLL_READY
-                        completer.complete(pollResult);
-                    } else {
-                        poll();
-                    }
-                }
-                callback = NativeCallable<UniffiRustFutureContinuationCallback>.listener(onResponse);
-
-                try {
-                    poll();
-                    await completer.future;
-                    callback.close();
-
-                    final status = calloc<RustCallStatus>();
-                    try {
-                        final result = completeFunc(rustFuture, status);
-                        final errorHandler = UniffiRustCallStatusErrorHandler();
-                        errorHandler.checkStatus(status.ref);
-                        return liftFunc(result);
-                    } finally {
-                        calloc.free(status);
-                    }
-                } finally {
-                    freeFunc(rustFuture);
-                }
+            void ensureInitialized() {
+                _UniffiLib._checkApiVersion();
+                _UniffiLib._checkApiChecksums();
             }
         }
     }
 }
 
-// ... (rest of the file remains the same)
+pub struct DartBindingGenerator;
 
+impl BindingGenerator for DartBindingGenerator {
+    type Config = Config;
+
+    fn write_bindings(
+        &self,
+        ci: &ComponentInterface,
+        config: &Self::Config,
+        out_dir: &Utf8Path,
+        _try_format_code: bool,
+    ) -> Result<()> {
+        let filename = out_dir.join(format!("{}.dart", config.cdylib_name()));
+        let tokens = DartWrapper::new(ci, config).generate();
+        let file = std::fs::File::create(filename)?;
+
+        let mut w = fmt::IoWriter::new(file);
+
+        let fmt = fmt::Config::from_lang::<Dart>().with_indentation(fmt::Indentation::Space(4));
+        let config = dart::Config::default();
+
+        tokens.format_file(&mut w.as_formatter(&fmt), &config)?;
+        Ok(())
+    }
+    fn check_library_path(
+        &self,
+        _library_path: &Utf8Path,
+        _cdylib_name: Option<&str>,
+    ) -> Result<()> {
+        // FIXME: not sure what to check for here...?
+        Ok(())
+    }
+}
+
+pub fn generate_dart_bindings(
+    udl_file: &Utf8Path,
+    config_file_override: Option<&Utf8Path>,
+    out_dir_override: Option<&Utf8Path>,
+    library_file: Option<&Utf8Path>,
+) -> Result<()> {
+    uniffi_bindgen::generate_external_bindings(
+        &DartBindingGenerator {},
+        udl_file,
+        config_file_override,
+        out_dir_override,
+        library_file,
+        None,
+        true,
+    )
+}
