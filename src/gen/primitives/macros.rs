41c649553130bac617fcc621c048137dfe62f4b5
chavic <dev.chavic@gmail.com>
2024-06-29 11:46:06 +0200

    using static for primitives

--- src/gen/primitives/macros.rs
+++ src/gen/primitives/macros.rs
@@ -42,134 +42,48 @@ macro_rules! impl_code_type_for_primitive {
     };
 }
 
-
 macro_rules! impl_renderable_for_primitive {
     ($T:ty, $class_name:literal, $canonical_name:literal, $allocation_size:literal) => {
         impl Renderable for $T {
             fn render_type_helper(&self, _type_helper: &dyn TypeHelperRenderer) -> dart::Tokens {
-                // TODO: Need to modify behavior to allow
-                // if (type_helper.check($canonical_name)) {
-                //     return quote!()
-                // }
-                // This method can be expanded to generate type helper methods if needed.
-                let mut endian = (if $canonical_name.contains("Float") {
-                    "Endian.little"
+                use uniffi_bindgen::backend::CodeType;
+                let endian = (if $canonical_name.contains("Float") {
+                    ", Endian.little"
                 } else {
-                    "Endian.big"
-                });
-                let _final_uintlist = (if $canonical_name.contains("Float") {
-                    String::from($canonical_name) + "List.fromList(buf.reversed.toList())"
-                } else {
-                    String::from($canonical_name) + "List.fromList(buf.toList())"
+                    ""
                 });
 
-                let cl_name =  format!("FfiConverter{}", $canonical_name);
-                let data_type = &$canonical_name
-                    .replace("UInt", "Uint")
-                    .replace("Double", "Float");
-                let type_signature = if data_type.contains("Float") {
-                    "double"
-                } else {
-                    endian = "";
-                    "int"
-                };
+                let cl_name = &self.ffi_converter_name();
+                let type_signature = &self.type_label();
+                let conversion_name = &$canonical_name
+                                    .replace("UInt", "Uint")
+                                    .replace("Double", "Float");
 
                 quote! {
-                    class $cl_name extends FfiConverter<$type_signature, $type_signature> 
-                    with FfiConverterPrimitive<$type_signature> {
-                        @override
-                        $type_signature read(ByteData buffer, int offset) => buffer.get$data_type(offset);
+                    class $cl_name {
+                        static $type_signature lift(RustBuffer buf) {
+                            return $cl_name.read(buf.asUint8List()).value;
+                        }
+                        static LiftRetVal<$type_signature> read(Uint8List buf) {
+                            return LiftRetVal(buf.buffer.asByteData(buf.offsetInBytes).get$conversion_name(0), $allocation_size);
+                        }
 
-                        @override
-                        int size([$type_signature value = $allocation_size]) {
+                        static RustBuffer lower($type_signature value) {
+                            final buf = Uint8List($cl_name.allocationSize(value));
+                            final byteData = ByteData.sublistView(buf);
+                            byteData.set$conversion_name(0, value$endian);
+                            return toRustBuffer(Uint8List.fromList(buf.toList()));
+                        }
+
+                        static int allocationSize([$type_signature value = 0]) {
                           return $allocation_size;
                         }
 
-                        @override
-                        void write($type_signature value, ByteData buffer, int offset) =>
-                        buffer.set$data_type(offset, value);
-                    }
-                }
-            }
-        }
-    };
+                        static int write($type_signature value, Uint8List buf) {
+                            buf.buffer.asByteData(buf.offsetInBytes).set$conversion_name(0, value$endian);
+                            return $cl_name.allocationSize();
+                        }
 
-    (BooleanCodeType) => {
-        impl Renderable for BooleanCodeType {
-            fn render_type_helper(&self, _type_helper: &dyn TypeHelperRenderer) -> dart::Tokens {
-                // if (type_helper.check($canonical_name)) {
-                //     return quote!()
-                // }
-                // This method can be expanded to generate type helper methods if needed.
-                quote! {
-                    class FfiConverterBool implements FfiConverter<bool, int> {
-                        const FfiConverterBool();
-        
-                        @override
-                        bool lift(int value) => value != 0;
-        
-                        @override
-                        int lower(bool value) => value ? 1 : 0;
-        
-                        @override
-                        bool read(ByteData buffer, int offset) => buffer.getInt8(offset) != 0;
-        
-                        @override
-                        void write(bool value, ByteData buffer, int offset) {
-                        buffer.setInt8(offset, lower(value));
-                        }
-        
-                        @override
-                        int size(value) => 1;
-                    }
-                }
-            }
-        }
-    };
-
-    (StringCodeType) => {
-        impl Renderable for StringCodeType {
-            fn render_type_helper(&self, _type_helper: &dyn TypeHelperRenderer) -> dart::Tokens {
-                // This method can be expanded to generate type helper methods if needed.
-                quote! {
-                    // if (type_helper.check($canonical_name)) {
-                    //     return quote!()
-                    // }
-                    class FfiConverterString implements FfiConverter<String, RustBuffer> {
-                        const FfiConverterString();
-                        // TODO: Figure out why there's spooky behavior here, default should be four, will fix later
-                        String lift(RustBuffer value, [int offset = 0]) {
-                            try {
-                                final data = value.asTypedList().buffer.asUint8List(offset);
-                                return utf8.decoder.convert(data);
-                            } finally {
-                                value.free();
-                            }
-                        }
-        
-                        @override
-                        RustBuffer lower(String value) {
-                            final buffer = toRustBuffer(Utf8Encoder().convert(value)); // TODO: Fix the meme copies issue by first fixing read
-                            return buffer;
-                        }
-        
-                        @override
-                        String read(ByteData buffer, int offset) {
-                            // TODO! : Fix this, it shouldn't append the lenth to every string, please remove first four bytes later
-                            final length = buffer.getInt32(offset);
-                            final stringBytes = buffer.buffer.asUint8List(offset + 4, length);
-                            return utf8.decoder.convert(stringBytes);
-                        }
-        
-                        @override
-                        void write(String value, ByteData buffer, int offset) {
-                        final stringBytes = utf8.encode(value);
-                        buffer.setInt32(offset, stringBytes.length);
-                        buffer.buffer.asUint8List(offset + 4).setAll(0, stringBytes);
-                        }
-        
-                        @override
-                        int size(value) => 4 + utf8.encode(value).length;
                     }
                 }
             }
@@ -186,8 +100,8 @@ macro_rules! impl_renderable_for_primitive {
                 quote! {
                     class BytesFfiConverter extends FfiConverter<$canonical_name, RustBuffer> {
                         @override
-                        int lift(RustBuffer buf, [int offset = 0]) {
-                            // final uint_list = buf.asUint8List();
+                        LiftRetVal<int> read(Uint8List buf) {
+                            // final uint_list = buf.toIntList();
                             // return uint_list.buffer.asByteData().get$canonical_name(1);
                         }
 
@@ -207,7 +121,7 @@ macro_rules! impl_renderable_for_primitive {
                         }
 
                         @override
-                        int size([T value]) {
+                        int allocationSize([T value]) {
                         //   return $allocation_size; // 1 = 8bits//TODO: Add correct allocation size for bytes, change the arugment type
                         }
 
