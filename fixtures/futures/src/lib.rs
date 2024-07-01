69ed1b6a6b9cff087408b24c25cdbb0e3177893a
chavic <dev.chavic@gmail.com>
2024-07-01 13:46:43 +0200

    uncommented tests

--- fixtures/futures/src/lib.rs
+++ fixtures/futures/src/lib.rs
@@ -60,63 +60,63 @@ impl TimerFuture {
     }
 }
 
-// /// Non-blocking timer future.
-// pub struct BrokenTimerFuture {
-//     shared_state: Arc<Mutex<SharedState>>,
-// }
+/// Non-blocking timer future.
+pub struct BrokenTimerFuture {
+    shared_state: Arc<Mutex<SharedState>>,
+}
 
-// impl Future for BrokenTimerFuture {
-//     type Output = ();
+impl Future for BrokenTimerFuture {
+    type Output = ();
 
-//     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-//         let mut shared_state = self.shared_state.lock().unwrap();
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut shared_state = self.shared_state.lock().unwrap();
 
-//         if shared_state.completed {
-//             Poll::Ready(())
-//         } else {
-//             shared_state.waker = Some(cx.waker().clone());
-//             Poll::Pending
-//         }
-//     }
-// }
+        if shared_state.completed {
+            Poll::Ready(())
+        } else {
+            shared_state.waker = Some(cx.waker().clone());
+            Poll::Pending
+        }
+    }
+}
 
-// impl BrokenTimerFuture {
-//     pub fn new(duration: Duration, fail_after: Duration) -> Self {
-//         let shared_state = Arc::new(Mutex::new(SharedState {
-//             completed: false,
-//             waker: None,
-//         }));
+impl BrokenTimerFuture {
+    pub fn new(duration: Duration, fail_after: Duration) -> Self {
+        let shared_state = Arc::new(Mutex::new(SharedState {
+            completed: false,
+            waker: None,
+        }));
 
-//         let thread_shared_state = shared_state.clone();
+        let thread_shared_state = shared_state.clone();
 
-//         // Let's mimic an event coming from somewhere else, like the system.
-//         thread::spawn(move || {
-//             thread::sleep(duration);
+        // Let's mimic an event coming from somewhere else, like the system.
+        thread::spawn(move || {
+            thread::sleep(duration);
 
-//             let mut shared_state: MutexGuard<_> = thread_shared_state.lock().unwrap();
-//             shared_state.completed = true;
+            let mut shared_state: MutexGuard<_> = thread_shared_state.lock().unwrap();
+            shared_state.completed = true;
 
-//             if let Some(waker) = shared_state.waker.take() {
-//                 // Do not consume `waker`.
-//                 waker.wake_by_ref();
+            if let Some(waker) = shared_state.waker.take() {
+                // Do not consume `waker`.
+                waker.wake_by_ref();
 
-//                 // And this is the important part. We are going to call
-//                 // `wake()` a second time. That's incorrect, but that's on
-//                 // purpose, to see how foreign languages will react.
-//                 if fail_after.is_zero() {
-//                     waker.wake();
-//                 } else {
-//                     thread::spawn(move || {
-//                         thread::sleep(fail_after);
-//                         waker.wake();
-//                     });
-//                 }
-//             }
-//         });
+                // And this is the important part. We are going to call
+                // `wake()` a second time. That's incorrect, but that's on
+                // purpose, to see how foreign languages will react.
+                if fail_after.is_zero() {
+                    waker.wake();
+                } else {
+                    thread::spawn(move || {
+                        thread::sleep(fail_after);
+                        waker.wake();
+                    });
+                }
+            }
+        });
 
-//         Self { shared_state }
-//     }
-// }
+        Self { shared_state }
+    }
+}
 
 #[uniffi::export]
 pub fn greet(who: String) -> String {
@@ -131,19 +131,19 @@ pub async fn always_ready() -> bool {
 #[uniffi::export]
 pub async fn void_function() {}
 
-// #[uniffi::export]
-// pub async fn say() -> String {
-//     TimerFuture::new(Duration::from_secs(2)).await;
+#[uniffi::export]
+pub async fn say() -> String {
+    TimerFuture::new(Duration::from_secs(2)).await;
 
-//     "Hello, Future!".to_string()
-// }
+    "Hello, Future!".to_string()
+}
 
-// #[uniffi::export]
-// pub async fn say_after(ms: u16, who: String) -> String {
-//     TimerFuture::new(Duration::from_millis(ms.into())).await;
+#[uniffi::export]
+pub async fn say_after(ms: u16, who: String) -> String {
+    TimerFuture::new(Duration::from_millis(ms.into())).await;
 
-//     format!("Hello, {who}!")
-// }
+    format!("Hello, {who}!")
+}
 
 #[uniffi::export]
 pub async fn sleep(ms: u16) -> bool {
@@ -153,22 +153,21 @@ pub async fn sleep(ms: u16) -> bool {
 }
 
 // Our error.
-// Our error.
-// #[derive(uniffi::Error, Debug)]
-// pub enum MyError {
-//     Foo,
-// }
+#[derive(thiserror::Error, uniffi::Error, Debug)]
+pub enum MyError {
+    #[error("Foo")]
+    Foo,
+}
 
 // An async function that can throw.
-// An async function that can throw.
-// #[uniffi::export]
-// pub async fn fallible_me(do_fail: bool) -> Result<u8, MyError> {
-//     if do_fail {
-//         Err(MyError::Foo)
-//     } else {
-//         Ok(42)
-//     }
-// }
+#[uniffi::export]
+pub async fn fallible_me(do_fail: bool) -> Result<u8, MyError> {
+    if do_fail {
+        Err(MyError::Foo)
+    } else {
+        Ok(42)
+    }
+}
 
 #[uniffi::export(async_runtime = "tokio")]
 pub async fn say_after_with_tokio(ms: u16, who: String) -> String {
@@ -176,24 +175,24 @@ pub async fn say_after_with_tokio(ms: u16, who: String) -> String {
     format!("Hello, {who} (with Tokio)!")
 }
 
-// #[derive(uniffi::Record)]
-// pub struct MyRecord {
-//     pub a: String,
-//     pub b: u32,
-// }
+#[derive(uniffi::Record)]
+pub struct MyRecord {
+    pub a: String,
+    pub b: u32,
+}
 
-// #[uniffi::export]
-// pub async fn new_my_record(a: String, b: u32) -> MyRecord {
-//     MyRecord { a, b }
-// }
+#[uniffi::export]
+pub async fn new_my_record(a: String, b: u32) -> MyRecord {
+    MyRecord { a, b }
+}
 
-// #[uniffi::export]
-// pub async fn broken_sleep(ms: u16, fail_after: u16) {
-//     BrokenTimerFuture::new(
-//         Duration::from_millis(ms.into()),
-//         Duration::from_millis(fail_after.into()),
-//     )
-//     .await;
-// }
+#[uniffi::export]
+pub async fn broken_sleep(ms: u16, fail_after: u16) {
+    BrokenTimerFuture::new(
+        Duration::from_millis(ms.into()),
+        Duration::from_millis(fail_after.into()),
+    )
+    .await;
+}
  
 uniffi::include_scaffolding!("api");
