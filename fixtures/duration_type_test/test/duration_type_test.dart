c6d2967e13d29af0105a85c26a46ba9c5eec8fe3
chavic <dev.chavic@gmail.com>
2024-07-13 11:47:49 +0200

    updated duration test

--- fixtures/duration_type_test/test/duration_type_test.dart
+++ fixtures/duration_type_test/test/duration_type_test.dart
@@ -2,34 +2,33 @@ import 'package:test/test.dart';
 import '../duration_type_test.dart';
 
 void main() {
-  final api = Api.load();
   test('rust return value seconds check', () {
-    final duration = api.makeDuration(5, 0);
+    final duration = makeDuration(5, 0);
 
     expect(duration.inSeconds, 5);
-    expect(api.getSeconds(duration), 5);
-    expect(api.getNanos(duration), 0);
+    expect(getSeconds(duration), 5);
+    expect(getNanos(duration), 0);
   });
 
   test('seconds data check from dart', () {
     final duration = Duration(seconds: 10);
-    expect(api.getSeconds(duration), 10);
-    expect(api.getNanos(duration), 0);
+    expect(getSeconds(duration), 10);
+    expect(getNanos(duration), 0);
   });
 
   test('check nanos/micros', () {
-    final duration = api.makeDuration(0, 3000);
+    final duration = makeDuration(0, 3000);
     expect(duration.inSeconds, 0);
     expect(duration.inMicroseconds, 3);
-    expect(api.getSeconds(duration), 0);
-    expect(api.getNanos(duration), 3000);
+    expect(getSeconds(duration), 0);
+    expect(getNanos(duration), 3000);
   });
 
   test('check large values', () {
-    final duration = api.makeDuration(123456789, 3000000);
+    final duration = makeDuration(123456789, 3000000);
     expect(duration.inSeconds, 123456789);
     expect(duration.inMicroseconds, 123456789003000);
-    expect(api.getSeconds(duration), 123456789);
-    expect(api.getNanos(duration), 3000000);
+    expect(getSeconds(duration), 123456789);
+    expect(getNanos(duration), 3000000);
   });
 }
