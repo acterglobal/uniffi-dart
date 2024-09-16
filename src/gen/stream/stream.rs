0349566f1e6f1f8b3d96f351068730865f37dd80
chavic <dev.chavic@gmail.com>
2024-09-16 13:20:02 +0200

    refactored to use dispose and finalizer

--- src/gen/stream/stream.rs
+++ src/gen/stream/stream.rs
@@ -4,29 +4,28 @@ use uniffi_bindgen::interface::Object;
 use crate::gen::oracle::DartCodeOracle;
 use crate::gen::render::TypeHelperRenderer;
 
-
 pub fn generate_stream(obj: &Object, _type_helper: &dyn TypeHelperRenderer) -> dart::Tokens {
     let obj_name = obj.name();
-    let obj_var_name = &DartCodeOracle::var_name(obj_name);
     let fn_name = DartCodeOracle::fn_name(&obj_name.replace("StreamExt", ""));
+    let obj_var_name = &DartCodeOracle::var_name(&fn_name);
     let create_obj_fn_name = format!("createStream{}", &obj_name.replace("StreamExt", ""));
 
     quote! {
-        final $obj_var_name = $create_obj_fn_name();
-
         $fn_name() async* {
+            final $obj_var_name = $create_obj_fn_name();
             try {
                 while (true) {
-                final value = await $obj_var_name.pollNext();
-                print(value);
-                if (value == null) {
-                    break;
+                    final value = await $obj_var_name.next();
+                    if (value == null) {
+                        break;
+                    }
+                    yield value;
                 }
-                yield value;
-                }
-            } finally {
-                $obj_var_name.drop();
+            } catch (e) {
+                // Handle exceptions if necessary
+                rethrow;
             }
+            // No need to call drop(), Finalizer will handle it
         }
     }
 }
